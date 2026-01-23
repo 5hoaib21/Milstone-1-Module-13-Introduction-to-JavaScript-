da49fc1b37298767cc111915b72a2911c7903c9f
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-01-23 06:44:08 +0600

    some practices

--- naming.js
+++ naming.js
@@ -1,5 +1,1 @@
-var false = 45;
-var var = 50;
 
-
-var is happy = true; //not allowed
---
+++ number.js
@@ -0,0 +1,7 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = 60;
+
+console.log(typeof orangePrice);
+console.log(typeof chocolate);
+console.log(typeof applePrice);
