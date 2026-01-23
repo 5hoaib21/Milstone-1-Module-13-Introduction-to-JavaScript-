122aa3952c4597736eba28eb6bafdc2c14a723ed
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-01-23 07:07:21 +0600

    first

--- number.js
+++ number.js
@@ -1,7 +1,12 @@
 var orangePrice = 20;
 var chocolate = 0.5;
-var applePrice = 60;
+// var applePrice = parseInt("220.5");
+var applePrice = parseFloat("22.5");
+console.log(orangePrice);
+console.log(applePrice);
+console.log(orangePrice + applePrice);
 
-console.log(typeof orangePrice);
-console.log(typeof chocolate);
-console.log(typeof applePrice);
+// console.log(applePrice);
+
+// const varPrice = 90;
+// console.log(varPrice);
